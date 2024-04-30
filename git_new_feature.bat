f243b8ba806f81508a49016458d5ee5056d930a7
DurpBawlWelf <durpbawlwelf@gmail.com>
2024-04-30 11:49:37 +0100

    A

--- git_new_feature.bat
+++ git_new_feature.bat
@@ -1,8 +1,7 @@
 D:
 cd D:\GODOT_PROJECTS\git_test
-git init
+git branch new-feature
+git checkout new-feature
 git add .
-git commit -m "Initial commit"
-git remote add origin https://github.com/DurpBawlWelf/godot_test.git
-git branch -M main
-git push -u origin main
+git commit -m "message"
+git push --set-upstream origin new-feature
