6d52ffe22b2eb707b99d6a957b70a68a3c5bca6b
DurpBawlWelf <durpbawlwelf@gmail.com>
2024-04-30 11:39:22 +0100

    message

---
+++ git_first.bat
@@ -0,0 +1,8 @@
+D:
+cd D:\GODOT_PROJECTS\git_test
+git init
+git add .
+git commit -m "Initial commit"
+git remote add origin https://github.com/DurpBawlWelf/godot_test.git
+git branch -M main
+git push -u origin main
